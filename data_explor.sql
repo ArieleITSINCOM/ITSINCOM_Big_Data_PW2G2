27db66d40f7cc4a3ab4c81f677c648d575ab3c4c
ArieleITSINCOM <ariele.bernasconi@itsincom.it>
2024-01-29 10:08:11 +0100

    updated data explor

--- data_explor.sql
+++ data_explor.sql
@@ -5,6 +5,7 @@
     - The quintiles of the SDI index are used to define low (~20), low-middle (~40), middle (~60), middle-high (~80) and high (~100) SDI countries in 201927
     - 
 */
+
 --------------------------------------------------------------------------------------------------------------------------------------------
 /*stg.BC_nation1*/
 -- SELECT TOP 8000 * FROM [stg].[BC_nation1] ORDER BY NEWID(), year -- 51.408 rows
@@ -29,8 +30,9 @@ measure: what the row means e.g. deaths, incidence, rate
 --------------------------------------------------------------------------------------------------------------------------------------------
 
 /*[stg].[BC_Percent1]*/
--- SELECT * FROM stg.bc_percent1 -- 5.040 rows
-/*
+-- SELECT * FROM [stg].[bc_percent1] -- 5.040 rows
+-- SELECT * FROM [dwh].[percent1]
+/*-
 for each measure (DALYs, Deaths), calculates number, percent, rate using ALL ages on based on sdi location and time-wise on a (likely) 6-month basis
 
     - measure: DALY, anni di "vita sana" persi per malattia https://www.who.int/data/gho/indicator-metadata-registry/imr-details/158
@@ -76,7 +78,7 @@ for each year and for each location death and incidence are measured, number and
 	- age:
         - all ages: s.e.
         - Age-standardized: age is processed in ways that makes ages more comparable
-    
+
 */
 
 --------------------------------------------------------------------------------------------------------------------------------------------
